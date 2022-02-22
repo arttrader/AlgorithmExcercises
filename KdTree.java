d338610b265a179e50aa3ef50919cb664e246084
arttrader <arttradej@gmail.com>
2022-02-22 16:11:44 +0900

    KdTree - missing size() added

--- KdTree.java
+++ KdTree.java
@@ -16,28 +16,29 @@ public class KdTree {
     private Node root;
 
     private class Node {
-        Point2D key;
-        Node left, right;
-        boolean vertical;
+        private Point2D key;
+        private Node left, right;
+        private boolean vertical;
+        private int size;
 
         public Node(Point2D key, boolean vert) {
             this.key = key;
             this.vertical = vert;
+            size = 0;
         }
-    }
-
-    private Node put(Node x, Point2D p, boolean vert) {
-        if (x == null) return new Node(p, vert);
-        int cmp;
-        if (x.vertical) cmp = Point2D.X_ORDER.compare(p, x.key);
-        else cmp = Point2D.Y_ORDER.compare(p, x.key);
-        if (cmp < 0) x.left = put(x.left, p, !vert);
-        else x.right = put(x.right, p, !vert);
-        return x;
     }
 
     public boolean isEmpty() {
         return (root == null);
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.size;
     }
 
     private boolean contains(Point2D p, Node x) {
@@ -60,14 +61,25 @@ public class KdTree {
         else return contains(p, root);
     }
 
+    private Node put(Node x, Point2D p, boolean vert) {
+        if (x == null) return new Node(p, vert);
+        int cmp;
+        if (x.vertical) cmp = Point2D.X_ORDER.compare(p, x.key);
+        else cmp = Point2D.Y_ORDER.compare(p, x.key);
+        if (cmp < 0) x.left = put(x.left, p, !vert);
+        else x.right = put(x.right, p, !vert);
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
         root = put(root, p, true);
     }
 
     private ArrayList<Point2D> range(RectHV rect, Node x) {
-        if (x == null) return null;
         ArrayList<Point2D> r = new ArrayList<>();
+        if (x == null) return r;
         if (rect.contains(x.key)) r.add(x.key);
         if (x.vertical)
             if (x.key.x() > rect.xmax())
@@ -79,7 +91,6 @@ public class KdTree {
                 r.addAll(range(rect, x.left));
             else
                 r.addAll(range(rect, x.right));
-
         return r;
     }
 

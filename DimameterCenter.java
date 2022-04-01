a6047de05d1a532f23f7e8bc1a674d8c4a68f8ad
arttrader <arttradej@gmail.com>
2022-04-01 10:04:13 +0900

    Euler Cycle

--- DimameterCenter.java
+++ DimameterCenter.java
@@ -7,32 +7,63 @@
 import edu.princeton.cs.algs4.BreadthFirstPaths;
 import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DimameterCenter {
     private final Graph g;
     private final BreadthFirstPaths bfp;
+    private final int s;
 
     public DimameterCenter(Graph G, int s) {
         g = new Graph(G);
         bfp = new BreadthFirstPaths(g, s);
+        this.s = s;
     }
 
     public Iterable<Integer> diameter() {
         // for each vertex
         // find distance to each vertex
         // return the longest
-
-
-        return null;
+        int v = g.V();
+        int e = g.E();
+        int longest = 0;
+        int longestV = s;
+        int longest2 = 0;
+        int longestV2 = s;
+        for (int i = 0; i < v; i++) {
+            int d = bfp.distTo(i);
+            if (d > longest) {
+                longest2 = longest;
+                longestV2 = i;
+                longest = d;
+                longestV = i;
+            }
+        }
+        Iterable<Integer> list = bfp.pathTo(longestV);
+        Iterable<Integer> list2 = bfp.pathTo(longestV2);
+        List<Integer> result = new ArrayList<>();
+        for (int i : list) result.add(i);
+        Stack<Integer> reverse = new Stack<>();
+        for (int i : list2) if (i != s) reverse.push(i);
+        for (int i : reverse) result.add(i);
+        return result;
     }
 
     public int center() {
-        // for each vertex
-        // find the longest distance to any vertex
-        // choose the one with the shortest
-
-
-        return 0;
+        // get diameter
+        // find vertex at diameter / 2
+        Iterable<Integer> d = diameter();
+        List<Integer> list = new ArrayList<>();
+        int len = 0;
+        for (int i: d) {
+            list.add(i);
+            len++;
+        }
+        int m = len / 2;
+        return list.get(m);
     }
 
 
---
+++ EulerCycle.java
@@ -0,0 +1,57 @@
+/* *****************************************************************************
+ *  Name:              J Hirota
+ *  Coursera User ID:
+ *  Last modified:     2022-3-31
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EulerCycle {
+    private final Graph g;
+    private boolean[] marked;
+
+    public EulerCycle(Graph G) {
+        g = new Graph(G);
+        marked = new boolean[G.E()];
+    }
+
+    public boolean eulerCycle() {
+        for (int i = 0; i < g.V(); i++) {
+            if (g.degree(i) % 2 != 0) return false;
+        }
+        return true;
+    }
+
+    private void findEulerPath(int v, List<Integer> list) {
+        for (int a: g.adj(v)) {
+            if (!marked[a]) {
+                marked[a] = true;
+                findEulerPath(a, list);
+            }
+        }
+        list.add(v);
+        marked[v] = true;
+    }
+
+    public Iterable<Integer> findEulerCycle() {
+        List<Integer> list = new ArrayList<>();
+        findEulerPath(0, list);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        EulerCycle e = new EulerCycle(G);
+        if (e.eulerCycle()) {
+            StdOut.println("Euler Cycle");
+            Iterable<Integer> it = e.findEulerCycle();
+            StdOut.println(it);
+        }
+    }
+}

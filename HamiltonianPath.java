53fb3dfecb3e80508e2a1fc2369137f00a33d6d5
arttrader <arttradej@gmail.com>
2022-04-27 09:01:55 +0900

    Create HamiltonianPath.java
    HamiltonianPath added

---
+++ HamiltonianPath.java
@@ -0,0 +1,85 @@
+/* *****************************************************************************
+ *  Name:              J Hirota
+ *  Coursera User ID:
+ *  Last modified:     2022-4-18
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HamiltonianPath {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private Stack<Integer> stack;
+    private List<Stack<Integer>> cycles;
+    private boolean[] onStack;
+
+    public HamiltonianPath(Digraph G) {
+        onStack = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        cycles = new ArrayList<>();
+        for (int v = 0; v < G.V(); v++)
+            if (!marked[v]) dfs(G, v);
+        int shortest = Integer.MAX_VALUE;
+        for (Stack<Integer> cycle : cycles)
+            if (cycle.size() < shortest) {
+                stack = cycle;
+                shortest = cycle.size();
+            }
+    }
+
+    private void dfs(Digraph G, int v) {
+        StdOut.println("Checking " + v);
+        onStack[v] = true;
+        marked[v] = true;
+        for (int w: G.adj(v))
+            if (this.hasCycle()) {
+                StdOut.println("Cycle found: length=" + stack.size());
+                for (int i: stack)
+                    StdOut.print(i + " ");
+                StdOut.println();
+                cycles.add(stack);
+                stack = null;
+                onStack[v] = false;
+                return;
+            } else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            } else if (onStack[w]) {
+                stack = new Stack<>();
+                for (int x = v; x != w; x = edgeTo[x])
+                    stack.push(x);
+                stack.push(w);
+                stack.push(v);
+            }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return stack != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return stack;
+    }
+
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int s = 0;
+        Digraph G = new Digraph(in);
+        HamiltonianPath dc = new HamiltonianPath(G);
+        StdOut.println("Has cycle: " + dc.hasCycle());
+        if (dc.hasCycle()) {
+            Iterable<Integer> cycle = dc.cycle();
+            for (int v: cycle)
+                StdOut.print(v + " ");
+        }
+    }
+}
